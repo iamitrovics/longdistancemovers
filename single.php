d5562ebd6446e5025ae7638e996ede3bc580a4c7
Isidora Kalicanin <isidorakalicanin@madeonline.agency>
2021-09-24 16:24:12 +0200

    author info update

--- single.php
+++ single.php
@@ -44,11 +44,16 @@ $container = get_theme_mod( 'understrap_container_type' );
                         <?php echo get_the_category_list(', '); ?>
                         
                             on <?php echo get_the_date( 'F j, Y' ); ?>
-                            <br>
-                            By <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>"><?php the_author(); ?></a>
+            
                         </span>
-
-                        
+                        <div class="author-desc">
+                            <?php echo get_avatar( get_the_author_meta( 'ID' ), 60 ); ?>
+                            <div class="author-content">
+                                <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>"><?php the_author(); ?></a>
+                                <p><?php the_author_description(); ?></p>
+                            </div>
+                            <!-- /.author-content -->
+                        </div>
 
                         <!-- /.blog-info -->
                         <h1><?php the_title(); ?></h1>
