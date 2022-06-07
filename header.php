80d3ca172074e40e96504e8753a735d83b06343d
Srdjan Mitrovic <iam@mitrovics.com>
2022-06-07 15:45:16 +0200

    update mobile menu

--- header.php
+++ header.php
@@ -52,86 +52,44 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 	<?php } ?>    
 
-    <div class="menu-overlay"></div>
-        <div class="main-menu-sidebar">
-            <header class="visible-xs visible-sm visible-md">
-                <a href="javascript:;" class="close-menu-btn">Close</a>
-            </header>
-            <!-- // header  -->        
-            <div id="mobile__brand">
-                <img src="<?php the_field('mobile_logo_white_general', 'options'); ?>" alt="<?php bloginfo('name'); ?>">
-            </div>
-            <!-- // brand  -->
-            <div id="menu">
 
-                <ul>
+	<div class="menu-overlay"></div>
+	<div class="main-menu-sidebar visible-xs visible-sm visible-md" id="menu">
 
-                     <?php if( have_rows('menu_items_mobile', 'options') ): ?>
-                        <?php while( have_rows('menu_items_mobile', 'options') ): the_row(); ?>
-                           ;
-                           
-                        <?php if (get_sub_field('link_type') == 'Single Item') { ?>
-                           <li><a href="<?php the_sub_field('link_to_page'); ?>"><?php the_sub_field('item_label'); ?> </a></li>
-                        <?php } elseif (get_sub_field('link_type') == 'Dropdown') { ?>
-
-                     
-                           <li>
-                        <a href="<?php the_sub_field('link_to_page'); ?>"><?php the_sub_field('item_label'); ?></a>
-                        <ul>
-                           <?php if( have_rows('dropdown_items') ): ?>
-                              <?php while( have_rows('dropdown_items') ): the_row(); ?>
-                                 <li><a href="<?php the_sub_field('link_to_page'); ?>"><?php the_sub_field('label'); ?></a></li>
-                              <?php endwhile; ?>
-                           <?php endif; ?>
-                        
-                        </ul>
-                     </li>               
-
-                    <?php } elseif (get_sub_field('link_type') == 'Dropdown Multilevel') { ?>
-                        
-                        <li>
-                            <a href="<?php the_sub_field('link_to_page'); ?>"><?php the_sub_field('item_label'); ?></a>
-                            <ul>
-
-                            <?php if( have_rows('multilevel_items') ): ?>
-                                <?php while( have_rows('multilevel_items') ): the_row(); ?>
+		<header>
+			<a href="javascript:;" class="close-menu-btn"><img src="<?php bloginfo('template_directory'); ?>/img/ico/close-x.svg" alt=""></a>
+		</header>
+		<!-- // header  -->
 
 
-                                        <?php if (get_sub_field('type_of_item') == 'Single Items') { ?>
+		<nav id="sidebar-menu-wrapper">
+			<img src="<?php the_field('website_logo_general', 'options'); ?>" alt="" class="mobile-logo">
+			<div id="menu">    
+				<ul class="nav-links">
+					<?php
+					wp_nav_menu( array(
+						'menu'              => 'mobile',
+						'theme_location'    => 'mobile',
+						'depth'             => 2,
+						'container'         => false,
+						'container_class'   => 'collapse navbar-collapse',
+						'container_id'      => false,
+						'menu_class'        => 'nav navbar-nav',
+						'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+						'items_wrap' => '%3$s',
+						'walker'            => new wp_bootstrap_navwalkermobile())
+					);
+					?>  
+				</ul>
+			</div>
+			<!-- // menu  -->
 
-                                            <li><a href="<?php the_sub_field('item_link'); ?>"><?php the_sub_field('item_label_sub'); ?></a></li>
+		</nav> 
+		<!-- // sidebar menu wrapper  -->
 
-                                        <?php } elseif (get_sub_field('type_of_item') == 'Dropdown Items') { ?>
-                                
-                                            <li>
-                                                <a href="<?php the_sub_field('item_link'); ?>"><?php the_sub_field('item_label_sub'); ?></a>
-                                                <ul>
-                                                    <?php if( have_rows('dropdown_items_sub') ): ?>
-                                                    <?php while( have_rows('dropdown_items_sub') ): the_row(); ?>
-                                                        <li><a href="<?php the_sub_field('link_sub_sub'); ?>"><?php the_sub_field('label_sub_sub'); ?></a></li>
-                                                    <?php endwhile; ?>
-                                                    <?php endif; ?>
-                                                </ul>
-                                            </li>
-
-                                        <?php } ?>      
-                                        <!-- // select of 3rd level    -->                  
-                                    
-                                <?php endwhile; ?>
-                            <?php endif; ?>
-                            </ul>
-                            
-                            </li>
-                        <?php } ?>   
-
-                    <?php endwhile; ?>
-                <?php endif; ?>
-
-                </ul>
-
-            </div>
-        </div>
-        <!-- // mobile menu  -->
+	</div>
+	<!-- // main menu sidebar  -->	
+    
         <header id="menu_area" class="menu-area">
             <div class="container">
                 <div class="row">
@@ -227,13 +185,17 @@ $container = get_theme_mod( 'understrap_container_type' );
 
                             </div>
                             <!-- /#top-call -->
-                            <div id="mobile-menu--btn">
+
+                            <div id="mobile-menu--btn" class="d-lg-none">
                                 <a href="javascript:;">
-                                <span></span>
-                                <span></span>
-                                <span></span>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                    <div class="clearfix"></div>
                                 </a>
                             </div>
+                            <!-- // mobile  -->	                            
+
                         </nav>
                     </div>
                 </div>
